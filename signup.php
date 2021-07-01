b77297c11bb0f0739db78d0fea4013934e4f5fd9
Shifanaaz02 <shifanaazsheikh02@gmail.com>
2021-07-01 17:35:53 +0530

    Signup modified

--- signup.php
+++ signup.php
@@ -35,105 +35,112 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <style>
     @import url(https://fonts.googleapis.com/css?family=Roboto:300);
 
-.login-page {
-  width: 360px;
-  padding: 8% 0 0;
-  margin: auto;
-}
-.form {
-  position: relative;
-  z-index: 1;
-  background: #FFFFFF;
-  max-width: 360px;
-  margin: 0 auto 100px;
-  padding: 45px;
-  text-align: center;
-  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-}
-.form input {
-  font-family: "Roboto", sans-serif;
-  outline: 0;
-  background: #f2f2f2;
-  width: 100%;
-  border: 0;
-  margin: 0 0 15px;
-  padding: 15px;
-  box-sizing: border-box;
-  font-size: 14px;
-}
+    .header {
+    padding: 30px;
+    text-align: center;
+    background: #FF616D;
+    color: white;
+    font-size: 60px;
+    font-family: "Georgia",Serif;
+    }
+    login-page {
+    width: 360px;
+    padding: 8% 0 0;
+    margin: auto;
+    }
+    form {
+    position: relative;
+    z-index: 1;
+    background: #FFFFFF;
+    max-width: 360px;
+    margin: 0 auto 100px;
+    padding: 45px;
+    text-align: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+    form input {
+    font-family: "Roboto", sans-serif;
+    outline: 0;
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+    }
 
-button {
-  background-color: #04AA6D;
-  color: red;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-.form button:hover,.form button:active,.form button:focus {
-  background: #FA9191;
-}
-.form .message {
-  margin: 15px 0 0;
-  color: #b3b3b3;
-  font-size: 12px;
-}
-.form .message a {
-  color: #FA9191;
-  text-decoration: none;
-}
-.form .register-form {
-  display: none;
-}
-.container {
-  position: relative;
-  z-index: 1;
-  max-width: 300px;
-  margin: 0 auto;
-}
-.container:before, .container:after {
-  content: "";
-  display: block;
-  clear: both;
-}
-.container .info {
-  margin: 50px auto;
-  text-align: center;
-}
-.container .info h1 {
-  margin: 0 0 15px;
-  padding: 0;
-  font-size: 36px;
-  font-weight: 300;
-  color: #1a1a1a;
-}
-.container .info span {
-  color: #4d4d4d;
-  font-size: 12px;
-}
-.container .info span a {
-  color: #000000;
-  text-decoration: none;
-}
-.container .info span .fa {
-  color:#FFBCBC;
-}
-body {
-  background: #FFBCBC; /* fallback for old browsers */
-  background: -webkit-linear-gradient(right, #FFBCBC, #F38BA0);
-  background: -moz-linear-gradient(right, #FFBCBC, #F38BA0);
-  background: -o-linear-gradient(right,#FFBCBC, #F38BA0);
-  background: linear-gradient(to left, #FFBCBC, #F38BA0);
-  font-family: "Roboto", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;      
-}
-
+    #button {
+    background-color: wheat;
+    color: red;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    }
+    form button:hover,.form button:active,.form button:focus {
+    background: #FA9191;
+    }
+    form message {
+    margin: 15px 0 0;
+    color: #b3b3b3;
+    font-size: 12px;
+    }
+    form message a {
+    color: #FA9191;
+    text-decoration: none;
+    }
+    form register-form {
+    display: none;
+    }
+    container {
+    position: relative;
+    z-index: 1;
+    max-width: 300px;
+    margin: 0 auto;
+    }
+    container:before, container:after {
+    content: "";
+    display: block;
+    clear: both;
+    }
+    container info {
+    margin: 50px auto;
+    text-align: center;
+    }
+    container info h1 {
+    margin: 0 0 15px;
+    padding: 0;
+    font-size: 36px;
+    font-weight: 300;
+    color: #1a1a1a;
+    }
+    container info span {
+    color: #4d4d4d;
+    font-size: 12px;
+    }
+    container info span a {
+    color: #000000;
+    text-decoration: none;
+    }
+    container info span fa {
+    color:#FF616D;
+    }
+    body {
+    background: #FF616D; /* fallback for old browsers */
+    background: -webkit-linear-gradient(right, #FF616D, #FF616D);
+    background: -moz-linear-gradient(right, #FF616D,#FF616D);
+    background: -o-linear-gradient(right,#FF616D, #FF616D);
+    background: linear-gradient(to left, #FF616D, #FF616D);
+    font-family: "Roboto", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;      
+    }
     </style>
   </head>
   <body>
-    <?php require 'partials/_nav.php' ?>
+    <!-- <?php require 'partials/_nav.php' ?> -->
     <?php
     if($showAlert){
     echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -152,9 +159,11 @@ body {
     </div> ';
     }
     ?>
-
+    <div class="header">
+    <h1 class="header">To Do List</h1>
+    </div>
     <div class="container my-4">
-     <h1 class="text-center">Signup to our website</h1>
+     <!-- <h1 class="text-center">Signup to our website</h1> -->
      <form action="/loginsystem/signup.php" method="post">
         <div class="form-group">
             <label for="username">Username</label>
@@ -171,8 +180,8 @@ body {
             <small id="emailHelp" class="form-text text-muted">Make sure to type the same password</small>
         </div>
          
-        <button type="submit" class="btn btn-primary">SignUp</button>
-        <p>Already have an account <a href="login.php">Login here</a></p>
+        <button type="submit" id="button" class="btn btn-primary">SignUp</button>
+        <p>Already have an account ?<a href="login.php">Login here</a></p>
      </form>
     </div>
     <!-- Optional JavaScript -->
